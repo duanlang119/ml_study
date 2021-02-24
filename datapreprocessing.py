730dcf9cb9e4282cd3877f599da4306260bbc2e2
gordonyaogz <gordonyaogz@gmail.com>
2021-02-24 11:14:54 +0800

    data pre processing

---
+++ datapreprocessing.py
@@ -0,0 +1,61 @@
+import numpy as np
+import pandas as pd
+
+'''
+st
+'''
+
+df = pd.read_csv('data/bank_churn_data.csv', delimiter=',')
+
+# Drop the columns as explained above
+df = df.drop(["RowNumber", "CustomerId", "Surname"], axis = 1)
+print(df.head())
+
+# Split Train, test data
+df_train = df.sample(frac=0.8,random_state=200)
+df_test = df.drop(df_train.index)
+print(len(df_train))
+print(len(df_test))
+
+
+df_train['BalanceSalaryRatio'] = df_train.Balance/df_train.EstimatedSalary
+# Given that tenure is a 'function' of age, we introduce a variable aiming to standardize tenure over age:
+df_train['TenureByAge'] = df_train.Tenure/(df_train.Age)
+'''Lastly we introduce a variable to capture credit score given age to take into account credit behaviour visavis adult life
+:-)'''
+df_train['CreditScoreGivenAge'] = df_train.CreditScore/(df_train.Age)
+
+
+
+# Arrange columns by data type for easier manipulation
+continuous_vars = ['CreditScore',  'Age', 'Tenure', 'Balance','NumOfProducts', 'EstimatedSalary', 'BalanceSalaryRatio',
+                   'TenureByAge','CreditScoreGivenAge']
+cat_vars = ['HasCrCard', 'IsActiveMember','Geography', 'Gender']
+df_train = df_train[['Exited'] + continuous_vars + cat_vars]
+print(df_train.head())
+
+'''For the one hot variables, we change 0 to -1 so that the models can capture a negative relation 
+where the attribute in inapplicable instead of 0'''
+df_train.loc[df_train.HasCrCard == 0, 'HasCrCard'] = -1
+df_train.loc[df_train.IsActiveMember == 0, 'IsActiveMember'] = -1
+print(df_train.head())
+
+# One hot encode the categorical variables
+lst = ['Geography', 'Gender']
+remove = list()
+for i in lst:
+    if (df_train[i].dtype == np.str or df_train[i].dtype == np.object):
+        for j in df_train[i].unique():
+            df_train[i+'_'+j] = np.where(df_train[i] == j,1,-1)
+        remove.append(i)
+df_train = df_train.drop(remove, axis=1)
+print(df_train.head())
+
+# minMax scaling the continuous variables
+minVec = df_train[continuous_vars].min().copy()
+maxVec = df_train[continuous_vars].max().copy()
+df_train[continuous_vars] = (df_train[continuous_vars]-minVec)/(maxVec-minVec)
+print(df_train.head())
+
+df_train.to_csv('data/df_train_data.csv', index=False)
+
