56d3eae9bf28fb0113d36688e16ca756ab384c25
gordonyaogz <gordonyaogz@gmail.com>
2021-05-26 19:10:39 +0800

    time serial Algorithems.

---
+++ newer_nn/chapter5/vision_study.py
@@ -0,0 +1,7 @@
+from torchvision import models, datasets,transforms,utils
+
+# print(list(filter(lambda x:False if x.startswith("__") else True, dir(models))))
+# print(list(filter(lambda x:False if x.startswith("__") else True, dir(datasets))))
+# print(list(filter(lambda x:False if x.startswith("__") else True, dir(transforms))))
+# print(list(filter(lambda x:False if x.startswith("__") else True, dir(utils))))
+
